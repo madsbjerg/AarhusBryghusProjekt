bf98468d7fbc76074446666f64c133fe6957914a
Mads Bjerg <au566699@uni.au.dk>
2022-03-29 13:38:18 +0200

    lmao made comment

--- src/Application/Models/Sampakning.java
+++ src/Application/Models/Sampakning.java
@@ -8,7 +8,7 @@ public class Sampakning extends Vare{
     private int antalOel; 
     private int antalGlas;
     private Drikkevare[] drikkevarer;
-    private int antalFyldt;
+    private int antalFyldt;                     // Hjælpe attribut til array.
 
     //private final ArrayList<Drikkevare> drikkevarer = new ArrayList<>();
 
