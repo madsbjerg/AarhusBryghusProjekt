21bdf25760117639b96fc33aa84f2e0f0c914485
Mads Bjerg <au566699@uni.au.dk>
2022-03-29 13:37:46 +0200

    made Sampakning use Array instead of arraylist

--- src/Application/Models/Sampakning.java
+++ src/Application/Models/Sampakning.java
@@ -7,15 +7,21 @@ public class Sampakning extends Vare{
     private String æsketype;
     private int antalOel; 
     private int antalGlas;
+    private Drikkevare[] drikkevarer;
+    private int antalFyldt;
 
-    private final ArrayList<Drikkevare> drikkevarer = new ArrayList<>();
+    //private final ArrayList<Drikkevare> drikkevarer = new ArrayList<>();
 
 
-    public Sampakning(String navn, int pant, String æsketype, int antalOel, int antalGlas){
+    public Sampakning(String navn, int pant, String æsketype, int antalOel, int antalGlas, int størrelse){
         super(navn, pant, Varetype.SAMPAKNING);
         this.æsketype = æsketype;
         this.antalOel = antalOel;
         this.antalGlas = antalGlas;
+        if(størrelse > 12) drikkevarer = new Drikkevare[12];
+        else if (størrelse < 2) drikkevarer = new Drikkevare[2];
+        else drikkevarer = new Drikkevare[størrelse];
+        antalFyldt = 0;
     }
 
     public String getÆsketype() {
@@ -35,14 +41,16 @@ public class Sampakning extends Vare{
     }
 
     public void removeDrikkevare(Drikkevare drikkevare){
-        if(drikkevarer.contains(drikkevare)){
-            drikkevarer.remove(drikkevare);
+        if(drikkevarer[0] != null){
+            drikkevarer[antalFyldt] = null;
+            antalFyldt--;
         }
     }
 
     public void addDrikkevare(Drikkevare drikkevare){
-        if(!drikkevarer.contains(drikkevare)){
-            drikkevarer.add(drikkevare);
+        if(drikkevarer.length > antalFyldt){
+            drikkevarer[antalFyldt] = drikkevare;
+            antalFyldt++;
         }
     }
 
