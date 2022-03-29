0ac898b2c8e2fe83e5338e25eb29d01e1b9ba197
Mads Bjerg <au566699@uni.au.dk>
2022-03-29 13:11:59 +0200

    fixed Diverse.java create methods

--- src/Application/Models/Diverse.java
+++ src/Application/Models/Diverse.java
@@ -9,16 +9,16 @@ public class Diverse extends Vare {
         this.beskrivelse = beskrivelse;
     }
 
-    public void createBeklædning(String navn, int pant, String beskrivelse){
-        new Diverse(navn, pant, Varetype.BEKLÆDNING, beskrivelse);
+    public void createBeklædning(String navn, String beskrivelse){
+        new Diverse(navn, 0, Varetype.BEKLÆDNING, beskrivelse);
     }
 
-    public void createMalt(String navn, int pant, String beskrivelse){
-        new Diverse(navn, pant, Varetype.MALT,beskrivelse);
+    public void createMalt(String navn, String beskrivelse){
+        new Diverse(navn, 0, Varetype.MALT,beskrivelse);
     }
 
-    public void createGlas(String navn, int pant, String beskrivelse){
-        new Diverse(navn, pant, Varetype.GLAS,beskrivelse);
+    public void createGlas(String navn, String beskrivelse){
+        new Diverse(navn, 0, Varetype.GLAS,beskrivelse);
     }
 
     public String getBeskrivelse(){
