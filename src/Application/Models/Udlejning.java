389042f27ae3b11829971b20ace64e4001d85418
Mike <eaamigk@students.eaaa.dk>
2022-04-01 13:04:13 +0200

    Udlejning ændret lidt

--- src/Application/Models/Udlejning.java
+++ src/Application/Models/Udlejning.java
@@ -12,8 +12,10 @@ public class Udlejning extends Salg{
     private LocalDate slutDato;
     private boolean betalt;
 
-    Udlejning(HashMap<Vare, Integer> varer, ArrayList<Udlejningsvare> udlejningsvare, LocalDate startDato, LocalDate slutDato){
-        super(varer,null, null);
+    Udlejning(HashMap<Vare, Integer> varer, ArrayList<Udlejningsvare> udlejningsvare, double pantBeloeb, LocalDate startDato, LocalDate slutDato,
+              Betalingsform betalingsform, Rabat rabat){
+        super(varer,betalingsform, rabat);
+        this.pantBeloeb = pantBeloeb;
         this.startDato = startDato;
         this.slutDato = slutDato;
         this.betalt = false;
