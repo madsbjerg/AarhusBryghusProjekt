2da7e764e28a30d1a6ea67ffe35925e6ce79f391
Omar <eaaomla@students.eaaa.dk>
2022-04-07 10:24:44 +0200

    lol

--- src/Gui/DagsopgørelsesPane.java
+++ src/Gui/DagsopgørelsesPane.java
@@ -81,7 +81,7 @@ public class DagsopgørelsesPane extends GridPane {
             new DateCell() {
             @Override public void updateItem(LocalDate item, boolean empty){
                 super.updateItem(item, empty);
-                    if(empty || datoer.contains(item) ){
+                    if(empty || datoer.contains(it  em) ){
                         setText(item.toString());
                     } else {
                         setText(null);
